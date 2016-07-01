0cdfaaee8763d523e9596b6fe8908b4dba983ffb
chswy123 <342423079@qq.com>
2016-07-01 13:05:07 +0800

    wy

---
+++ wxmanger.php
@@ -0,0 +1,10 @@
+<?php	
+	header("content-type:text/html;charset=utf-8");
+
+	define('APP_DEBUG',True);
+
+	define('APP_NAME','wxmanger');
+
+	define('APP_PATH','wxmanger/');
+
+	require('./ThinkPHP/ThinkPHP.php');
