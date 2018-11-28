3a081c545104eb9c79619c5566f4fe8f11045796
chswy123 <wangyue10@leju.com>
2018-11-28 23:54:20 +0800

    test

--- Mywechat/Home/Controller/IndexController.class.php
+++ Mywechat/Home/Controller/IndexController.class.php
@@ -7,43 +7,43 @@ class IndexController extends Controller{
 	public function __construct(){
 
 	}
-    
-    public function index(){
-       // echo 'this is a test'; exit;
 
-    	//获得参数 signature nonce token timestamp echostr
-    	$nonce = $_GET['nonce'];
-    	$token = 'weixin';
-    	$timestamp = $_GET['timestamp'];
-    	$echostr = $_GET['echostr'];
-    	$signature = $_GET['signature'];
+	public function index(){
+		// echo 'this is a test'; exit;
 
-    	//形成数组，然后字典排序
-    	$array = array();
-    	$array = array($nonce,$timestamp,$token);
-    	sort($array);
+		//获得参数 signature nonce token timestamp echostr
+		$nonce = $_GET['nonce'];
+		$token = 'weixin';
+		$timestamp = $_GET['timestamp'];
+		$echostr = $_GET['echostr'];
+		$signature = $_GET['signature'];
 
-    	//拼接成字符串，sha1加密，然后与signature进行校验
-    	$str = sha1(implode($array));
+		//形成数组，然后字典排序
+		$array = array();
+		$array = array($nonce,$timestamp,$token);
+		sort($array);
 
-    	if($str == $signature && $echostr){
-    		//第一次接入weixin api 接口的时候
-    		echo $echostr;
-    		exit;
-    	}
-    	else{
-    		$this->responseMsg();
-    	}
-    }
+		//拼接成字符串，sha1加密，然后与signature进行校验
+		$str = sha1(implode($array));
 
-    //接收事件推送并回复
-    public function responseMsg(){
-    	//接收到微信推送过来的post数据(XML格式)
-    	$postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+		if($str == $signature && $echostr){
+			//第一次接入weixin api 接口的时候
+			echo $echostr;
+			exit;
+		}
+		else{
+			$this->responseMsg();
+		}
+	}
 
-    	//处理消息类型，并设置回复类型和内容
+	//接收事件推送并回复
+	public function responseMsg(){
+		//接收到微信推送过来的post数据(XML格式)
+		$postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
 
-  //   		<xml>
+		//处理消息类型，并设置回复类型和内容
+
+		//   		<xml>
 		// <ToUserName><![CDATA[toUser]]></ToUserName>
 		// <FromUserName><![CDATA[FromUser]]></FromUserName>
 		// <CreateTime>123456789</CreateTime>
@@ -56,43 +56,43 @@ class IndexController extends Controller{
 		if(strtolower($postObj->MsgType)== 'event'){
 			if(strtolower($postObj->Event)== 'subscribe'){
 				$IndexModel = new \Model\IndexModel;
-                $IndexModel -> dingyEvent($postObj);
+				$IndexModel -> dingyEvent($postObj);
 			}
-            // else if($postObj->Event=='LOCATION'){
-            //     $toUser = $postObj->FromUserName;
-            //     $fromUser = $postObj->ToUserName;
-            //     $time = time();
-            //     $Msgtype = 'text';
-            //     $Content = '维度：'.$postObj->Latitude.'<br/>经度：'.$postObj->Longitude.'<br/>精度:'.$postObj->Precision;
-            //     $template = '<xml>
-            //                 <ToUserName><![CDATA[%s]]></ToUserName>
-            //                 <FromUserName><![CDATA[%s]]></FromUserName>
-            //                 <CreateTime>%s</CreateTime>
-            //                 <MsgType><![CDATA[%s]]></MsgType>
-            //                 <Content><![CDATA[%s]]></Content>
-            //                 </xml>';
-            //     $info = sprintf($template,$toUser,$fromUser,$time,$Msgtype,$Content);
-            //     echo $info;
-            // }
+			// else if($postObj->Event=='LOCATION'){
+			//     $toUser = $postObj->FromUserName;
+			//     $fromUser = $postObj->ToUserName;
+			//     $time = time();
+			//     $Msgtype = 'text';
+			//     $Content = '维度：'.$postObj->Latitude.'<br/>经度：'.$postObj->Longitude.'<br/>精度:'.$postObj->Precision;
+			//     $template = '<xml>
+			//                 <ToUserName><![CDATA[%s]]></ToUserName>
+			//                 <FromUserName><![CDATA[%s]]></FromUserName>
+			//                 <CreateTime>%s</CreateTime>
+			//                 <MsgType><![CDATA[%s]]></MsgType>
+			//                 <Content><![CDATA[%s]]></Content>
+			//                 </xml>';
+			//     $info = sprintf($template,$toUser,$fromUser,$time,$Msgtype,$Content);
+			//     echo $info;
+			// }
 		}
 
-        //用户发送特定消息服务号自动回复
-        if(strtolower($postObj->MsgType)=='text'){
-            //回复用户消息
-            $IndexModel = new \Model\IndexModel;
-            $IndexModel->repostMsg($postObj);
+		//用户发送特定消息服务号自动回复
+		if(strtolower($postObj->MsgType)=='text'){
+			//回复用户消息
+			$IndexModel = new \Model\IndexModel;
+			$IndexModel->repostMsg($postObj);
 
-        }
+		}
 
-    }
+	}
 
-    public function test(){
-        // print_r($_SERVER);exit;
-        // $test = new \Model\IndexModel;
-        echo '<pre>';
-        $test = new \Model\IndexModel();
-        $test->testMsg();
-    }
+	public function test(){
+		// print_r($_SERVER);exit;
+		// $test = new \Model\IndexModel;
+		echo '<pre>';
+		$test = new \Model\IndexModel();
+		$test->testMsg();
+	}
 
 
 
--- Mywechat/Home/Controller/TextController.class.php
+++ Mywechat/Home/Controller/TextController.class.php
@@ -27,6 +27,10 @@ class TextController extends Controller
             case 'admin':
                 $Content = 'http://www.v24php.cn/Mywechat.php/Admin/Login/login';
                 break;
+            case 'news':
+                $news = $this->news($toUser, $fromUser, $time);
+                echo $news;
+                break;
             default:
                 $Content = '要啥自行车';
                 break;
@@ -38,5 +42,43 @@ class TextController extends Controller
         echo $info;
 
     }
+
+
+
+    public function news($toUser, $fromUser, $time)
+    {
+        $Msgtype = 'news';
+
+        $tmplate = '<xml>
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[%s]]></MsgType>
+                        <ArticleCount>%s</ArticleCount>
+                        <Articles>
+                            <item>
+                            <Title><![CDATA[%s]]></Title>
+                            <Description><![CDATA[%s]]></Description>
+                            <PicUrl><![CDATA[%s]]></PicUrl>
+                            <Url><![CDATA[%s]]></Url>
+                            </item>
+                            <item>
+                            <Title><![CDATA[%s]]></Title>
+                            <Description><![CDATA[%s]]></Description>
+                            <PicUrl><![CDATA[%s]]></PicUrl>
+                            <Url><![CDATA[%s]]></Url>
+                            </item>
+                        </Articles>
+                    </xml>
+                    ';
+
+        $title = 'test';
+        $desc = 'this is test';
+        $picUrl = 'http://v24php.cn/Public/wx_img/2.jpg';
+        $url = 'http://www.v24php.cn/Mywechat.php/Admin/Login/login';
+        return sprintf($tmplate, $toUser, $fromUser, $time, $Msgtype, '2', $title, $desc, $picUrl, $url, $title, $desc, $picUrl, $url);
+    }
+
+
 
 }
